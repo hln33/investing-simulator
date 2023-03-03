8bfd32fbc8e61527b2a2300ea235bd6c021ef803
hln5 <harrynguyen019@gmail.com>
2023-03-03 13:58:46 -0800

    Add description of app to welcome page

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 function Welcome(props) {
     
-
     return (
         <div>
-            <h1>WELCOME!!!</h1>
+            <h1>Welcome to Investment Simulator</h1>
+            <br></br>
+
+            <h2>Practice Investing Without The Risks</h2>
+            <p>Practice investing without using real money and see how well you do.</p>
+            <br></br>
+
+            <h2>Real Time Data</h2>
+            <p>View hundreds of real life stocks and their peformance in real time</p>
+            <br></br>
+
+            <h2>Make Investing Fun</h2>
+            <p>Partcipate in investing competitions with other users to see who comes out on top.</p>
+            <br></br>
+
+            <p>Login</p>
+            <p>Don't have an account? <b>Sign Up</b></p>
         </div>
     );
 }
