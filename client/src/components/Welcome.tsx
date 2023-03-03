3eaece492dbfc96e408d8519a293c2897b3b583f
hln5 <harrynguyen019@gmail.com>
2023-03-03 15:17:34 -0800

    put cards in a flex grid

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { Card } from 'primereact/card';
 import { PrimeIcons } from 'primereact/api';
 
 import WelcomeCard from './WelcomeCard';
@@ -9,29 +8,34 @@ function Welcome(props) {
     
     return (
         <div>
-            <h1 style={{ color: 'var(--primary-color)' }}>Investment Simulator</h1>
-            <br></br>
+            <h1 style={{ color: 'var(--primary-color)', paddingLeft: '50px' }}>Investment Simulator</h1>
 
-            <WelcomeCard
-                title={"Practice Investing"}
-                subtitle={"Practice investing without using real money and see how well you do."}
-                icon={PrimeIcons.CHECK_SQUARE}
-            />
-            <br></br>
-
-            <WelcomeCard
-                title={"Compete with Others"}
-                subtitle={"Partcipate in investing competitions with other users to see who comes out on top."}
-                icon={PrimeIcons.USERS}
-            />
-            <br></br>
-
-            <WelcomeCard
-                title={"Real Time Data "}
-                subtitle={"View hundreds of real life stocks and their peformance in real time."}
-                icon={PrimeIcons.CHART_BAR}
-            />
-            <br></br>
+            <div className="card" style={{ paddingTop: '20px', paddingBottom: '120px', padding: '50px' }}>
+                <div className="flex card-container indigo-container">
+                    <div className="flex-1 h-4rem border-round">
+                        <WelcomeCard
+                            title={"Practice Investing"}
+                            subtitle={"Practice investing without using real money and see how well you do."}
+                            icon={PrimeIcons.CHECK_SQUARE}
+                        />
+                    </div>
+                    <div className="flex-1 h-4rem border-round mx-4">
+                        <WelcomeCard
+                            className="flex-1 h-4rem border-round mx-4"
+                            title={"Compete with Others"}
+                            subtitle={"Partcipate in investing competitions with other users to see who comes out on top."}
+                            icon={PrimeIcons.USERS}
+                        />
+                    </div>
+                    <div className="flex-1 h-4rem border-round">
+                        <WelcomeCard
+                            title={"Real Time Data "}
+                            subtitle={"View hundreds of real life stocks and their peformance in real time."}
+                            icon={PrimeIcons.CHART_BAR}
+                        />
+                    </div>
+                </div>
+            </div>
 
             <p>Login</p>
             <p>Don't have an account? <b>Sign Up</b></p>
