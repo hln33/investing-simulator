0e94ee364868a790a37f1b5cb29d01c34fc98e41
dnasol <dnasol@sfu.ca>
2023-03-03 23:14:14 -0800

    add react router and placeholder for login, dashboard, and register components

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from 'App';
+import { BrowserRouter } from 'react-router-dom';
+
+// Styling
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  // StrictMode is disabled to prevent default rendering twice
-  // <React.StrictMode>
+  <BrowserRouter>
     <App />
-  // </React.StrictMode>
+  </BrowserRouter>
 );
 
