3f558e7a7e1f0e6c2385adb2e242d2e82c26c8e0
Andy Cheng <andyc@virtro.ca>
2023-03-04 13:41:51 -0800

    split footer text into columns

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,8 +6,16 @@ function Footer() {
     return (
         <footer>
             <Divider/>
-            <div className='col=12'>
-                <p style={{ color: 'var(--primary-color)'}}>Investing Simulator for CMPT 372 Created by Group 24 Andy C. Mathew W. Harry N. Denzel N. </p>
+            <div className='grid'>
+                <div className='col-4' style={{textAlign:'center'}}>
+                    <p style={{ color: 'var(--primary-color)'}}>Investing Simulator for CMPT 372  </p>
+                </div>
+                <div className='col-4' style={{textAlign:'center'}}>
+                    <p style={{ color: 'var(--primary-color)'}}>Created by Group 24  </p>
+                </div>
+                <div className='col-4' style={{textAlign:'center'}}>
+                    <p style={{ color: 'var(--primary-color)'}}>Andy C. Mathew W. Harry N. Denzel N. </p>
+                </div>
             </div>
         </footer>
     );
