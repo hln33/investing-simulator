fa5f2fc04d062c1c1bf9396d34fb30dc5e48d631
dnasol <dnasol@sfu.ca>
2023-03-03 18:46:32 -0800

    merge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import axios from 'axios';
-
 import Welcome from './components/Welcome'
 
 import "primereact/resources/themes/bootstrap4-light-blue/theme.css";     //theme
