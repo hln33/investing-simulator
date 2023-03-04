075978f1317e6caab28a804fa0bf1f6938f4d588
dnasol <dnasol@sfu.ca>
2023-03-04 14:05:27 -0800

    refactor register form to use formik library

--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useFormik } from "formik";
+import { classNames } from "primereact/utils";
 
 // Components
 import Card from "components/PrimeReact/Card/Card";
@@ -11,45 +13,180 @@ import Button from "components/PrimeReact/Button/Button";
 import './style.scss';
 
 const Register = () => {
+
+  /** Formik **/
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phoneNumber: '',
+      password: '',
+      reEnterPassword: '',
+    },
+    validate: (data) => {
+      const errors = {} as any;
+
+      if (!data.firstName) errors.firstName = 'Name - First name is required.';
+
+      if (!data.lastName) errors.lastName = 'Last Name - Last name is required.';
+
+      if (!data.email) errors.email = 'Email - Email is required.';
+
+      if (!data.phoneNumber) errors.phoneNumber = 'Phone - Phone number is required.';
+
+      if (!data.password) errors.password = 'Password - Valid password is required.';
+
+      if (data.reEnterPassword !== data.password) errors.reEnterPassword = 'Password - Passwords must be the same.';
+
+      return errors;
+    },
+    onSubmit: (data) => {
+      /** @todo Do stuff with filled out form here */
+      formik.resetForm();
+    }
+  });
+
+  const isFormFieldInvalid = (name: string) => !!(formik.touched[name] && formik.errors[name]);
+
+  const getFormErrorMessage = (name: string) => {
+    return isFormFieldInvalid(name)
+      ? <small className="p-error">{formik.errors[name]}</small>
+      : <small className="p-error">&nbsp;</small>;
+  };
+
+  /** Components **/
+
+  const textFillerSection = (
+    <div className="hidden lg:block p-4 w-21rem h-8rem">
+      <p className="font-semibold text-5xl text-white">
+        Are you ready to dive into the world of investing?
+      </p>
+
+      <p className="font-medium text-3xl text-white">
+        Our simulation will give you the chance to experience the ups and downs of the stock market without risks
+      </p>
+    </div>
+  );
+
+  const startText = (
+    <p className="font-medium text-xl text-center">
+      Start with <span className="font-bold">$10,000</span> in your simulated investment portfolio today
+    </p>
+  );
+
+  const firstNameField = (
+    <>
+      <label htmlFor="first-name">First Name</label>
+      <InputText
+        className={`mt-1 w-full ${classNames({ 'p-invalid': isFormFieldInvalid('firstName') })}`}
+        inputId="first-name"
+        name="firstName"
+        value={formik.values.firstName}
+        onChange={(e: any) => formik.setFieldValue('firstName', e.target.value)}
+      />
+      {getFormErrorMessage('firstName')}
+    </>
+  );
+
+  const lastNameField = (
+    <>
+      <label htmlFor="last-name">Last Name</label>
+      <InputText
+        className={`mt-1 w-full ${classNames({ 'p-invalid': isFormFieldInvalid('lastName') })}`}
+        inputId="last-name"
+        name="lastName"
+        value={formik.values.lastName}
+        onChange={(e: any) => formik.setFieldValue('lastName', e.target.value)}
+      />
+      {getFormErrorMessage('lastName')}
+    </>
+  );
+
+  const emailField = (
+    <>
+      <label htmlFor="email">Email</label>
+      <InputText
+        className={`mt-1 w-full ${classNames({ 'p-invalid': isFormFieldInvalid('email') })}`}
+        id="email"
+        name="email"
+        value={formik.values.email}
+        onChange={(e: any) => formik.setFieldValue('email', e.target.value)}
+      />
+      {getFormErrorMessage('email')}
+    </>
+  );
+
+  const phoneNumberField = (
+    <>
+      <label htmlFor="phone">Phone</label>
+      <InputMask
+        className={`mt-1 w-full ${classNames({ 'p-invalid': isFormFieldInvalid('phoneNumber') })}`}
+        id="phone"
+        name="phoneNumber"
+        mask="(999) 999-9999"
+        placeholder="(999) 999-9999"
+        value={formik.values.phoneNumber}
+        onChange={(e: any) => formik.setFieldValue('phoneNumber', e.target.value)}
+      />
+      {getFormErrorMessage('phoneNumber')}
+    </>
+  );
+
+  const passwordField = (
+    <>
+      <label htmlFor="password">Password</label>
+      <Password
+        className={`register-password mt-1 w-full ${classNames({ 'p-invalid': isFormFieldInvalid('password') })}`}
+        inputId="password"
+        name="password"
+        value={formik.values.password}
+        onChange={(e: any) => formik.setFieldValue('password', e.target.value)}
+      />
+      {getFormErrorMessage('password')}
+
+    </>
+  );
+
+  const reEnterPasswordField = (
+    <>
+      <label htmlFor="re-enter-password">Re-enter Password</label>
+      <Password
+        className={`register-password mt-1 w-full ${classNames({ 'p-invalid': isFormFieldInvalid('reEnterpassword') })}`}
+        inputId="re-enter-password"
+        name="reEnterPassword"
+        value={formik.values.reEnterPassword}
+        onChange={(e: any) => formik.setFieldValue('reEnterPassword', e.target.value)}
+      />
+      {getFormErrorMessage('reEnterPassword')}
+    </>
+  );
+
   return (
     <div className="register flex h-screen w-screen">
       <div className="flex flex-grow-1 card-container flex-wrap justify-content-center align-content-center">
 
-        <div className="hidden lg:block p-4 w-21rem h-8rem">
-          <p className="font-semibold text-5xl text-white">
-            Are you ready to dive into the world of investing?
-          </p>
-
-          <p className="font-medium text-3xl text-white">
-            Our simulation will give you the chance to experience the ups and downs of the stock market without risks
-          </p>
-        </div>
+        {textFillerSection}
 
         <Card className="flex p-4 register-card align-items-center">
 
-            <p className="font-medium text-xl text-center">
-              Start with $10,000 in your simulated investment portfolio today
-            </p>
+          {startText}
 
-            <label htmlFor="first-name">First Name</label>
-            <InputText className="mb-2 mt-1 w-full" inputId="first-name" />
+          <form onSubmit={formik.handleSubmit}>
+            {firstNameField}
 
-            <label htmlFor="last-name">Last Name</label>
-            <InputText className="mb-2 mt-1 w-full" inputId="last-name" />
+            {lastNameField}
 
-            <label htmlFor="email">Email</label>
-            <InputText className="mb-2 mt-1 w-full" id="email" />
+            {emailField}
 
-            <label htmlFor="phone">Phone</label>
-            <InputMask className="mb-2 mt-1 w-full" id="phone" mask="(999) 999-9999" placeholder="(999) 999-9999" />
+            {phoneNumberField}
 
-            <label htmlFor="password">Password</label>
-            <Password className="register-password mb-2 mt-1 w-full" inputId="password" />
+            {passwordField}
 
-            <label htmlFor="re-enter-password">Re-enter Password</label>
-            <Password className="register-password mb-2 mt-1 w-full" inputId="re-enter-password" />
+            {reEnterPasswordField}
 
-            <Button className="w-full mt-2 h-4rem" label="Register Now" />
+            <Button className="w-full mt-2 h-4rem" type="submit" label="Register Now" />
+          </form>
 
         </Card>
 
