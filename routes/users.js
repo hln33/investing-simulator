94f391f84dc81faac35583b962bc38025894e385
hln5 <harrynguyen019@gmail.com>
2023-03-08 19:52:46 -0800

    these files should have been included in previous commit

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Validate a user's credentials  */
+router.get('/validate', function(req, res, next) {
+  res.send('validating');
+});
+
 module.exports = router;
