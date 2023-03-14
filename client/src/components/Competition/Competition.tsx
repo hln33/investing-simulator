e2d1aec9218d2d0760a99ba8e75282fc88df1770
hln5 <harrynguyen019@gmail.com>
2023-03-14 15:48:41 -0700

    create route for competition screen

---
+++ client/src/components/Competition/Competition.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Competition(props) {
+    
+    return (
+        <div>
+            <h1>Competition Screen</h1>
+        </div>
+    );
+}
+
+export default Competition
